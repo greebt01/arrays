3da854123bd35a6154a53753d140021c3ee758a0
Birk <rgeebt01@yahoo.com>
2022-04-19 14:12:20 -0400

    sorted the array

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 # Description
 Program made to display what can be done with arrays.  It counts the array length and returns the values as a string
 
+# Update1
+Sorts the array when it is returned to the command line.
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] numbers = {2, 3, 5, 0, 1};
+        Arrays.sort(numbers);
         System.out.println(numbers.length);
         System.out.println(Arrays.toString(numbers));
     }
